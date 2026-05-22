f2eed9b9db209f6bfefa289c766d168370054926
Diyali14 <diyali14082004@gmailcom>
2026-05-22 17:28:09 +0530

    Initialized Spring Boot Application

---
+++ hospitalinventory/hospitalinventory/src/main/java/com/inventory/hospitalinventory/HospitalinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.hospitalinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalinventoryApplication.class, args);
+	}
+
+}
---
+++ hospitalinventory/hospitalinventory/src/test/java/com/inventory/hospitalinventory/HospitalinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory.hospitalinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
